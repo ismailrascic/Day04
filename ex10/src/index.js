c61b0714426a3fde0b228130113bc6c70098ec1d
ismailrascic <ismaily.rascic@gmail.com>
2021-03-11 23:56:57 +0100

    index

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main()
+{
+var myFirstName = "Ismail";
+var myLastName = "Racic";
+var myHomeTown = "Sarajevo";
+
+return{ 
+myFirstName,
+myLastName,
+myHomeTown
+};
+
+
+
+}
+console.log(main());
+module.exports=main;
